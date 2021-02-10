349b8a925e839bc70f379449de1e0db8fa20dff1
DarrylBonnnicib43840 <56337637+DarrylBonnnicib43840@users.noreply.github.com>
2021-02-10 15:46:05 +0100

    Added Scripts Folder
    -MeshBuilder
    -TriangleMaker

---
+++ Scripts/TriangleMaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class TriangleMaker : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 size = Vector3.one;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		MeshFilter meshFilter = this.GetComponent<MeshFilter>();
+		MeshBuilder meshBuilder = new MeshBuilder(1);
+		
+		Vector3 p0 = new Vector3(size.x,  size.y, -size.z);
+		Vector3 p1 = new Vector3(-size.x, size.y, -size.z);
+		Vector3 p2 = new Vector3(-size.x, size.y,  size.z);
+		
+		meshBuilder.BuildTriangle(p0,p1,p2, 0);
+		
+		meshFilter.mesh = meshBuilder.CreateMesh();
+        
+    }
+}
