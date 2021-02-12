ef8f42c2d18ade1b7d404a1581b9440029c17034
DarrylBonnicib43840 <darryl.bonnici.b43840@mcast.edu.mt>
2021-02-12 19:39:21 +0100

    final commit

---
+++ Assets/Scripts/CubeMaker.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class CubeMaker : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 size = Vector3.one;
+	private List<Material> materialsList;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		MeshFilter meshFilter = this.GetComponent<MeshFilter>();
+		MeshBuilder meshBuilder = new MeshBuilder(6);
+		
+		//top vertices
+		 Vector3 t0 = new Vector3(size.x,	size.y,	-size.z);
+		 Vector3 t1 = new Vector3(-size.x,	size.y,	-size.z);
+		 Vector3 t2 = new Vector3(-size.x,	size.y,	size.z);
+		 Vector3 t3 = new Vector3(size.x,	size.y,	size.z);
+		 
+		 //bottom vertices
+		 Vector3 b0 = new Vector3(size.x,	-size.y,	-size.z);
+		 Vector3 b1 = new Vector3(-size.x,	-size.y,	-size.z);
+		 Vector3 b2 = new Vector3(-size.x,	-size.y,	size.z);
+		 Vector3 b3 = new Vector3(size.x,	-size.y,	size.z);
+		 
+		 //top square
+		 meshBuilder.BuildTriangle(t0, t1, t2, 0);
+		 meshBuilder.BuildTriangle(t0, t2, t3, 0);
+		 
+		 //bottom square
+		 meshBuilder.BuildTriangle(b2, b1, b0, 1);
+		 meshBuilder.BuildTriangle(b3, b2, b0, 1);
+		 
+		 //back sqaure
+		 meshBuilder.BuildTriangle(b0, t1, t0, 2);
+		 meshBuilder.BuildTriangle(b0, b1, t1, 2);
+		 
+		 //front square
+		 meshBuilder.BuildTriangle(b3, t0, t3, 3);
+		 meshBuilder.BuildTriangle(b3, b0, t0, 3);
+		 
+		 //left square
+		 meshBuilder.BuildTriangle(b1, t2, t1, 4);
+		 meshBuilder.BuildTriangle(b1, b2, t2, 4);
+		 
+		 //right square
+		 meshBuilder.BuildTriangle(b2, t3, t2, 5);
+		 meshBuilder.BuildTriangle(b2, b3, t3, 5);
+
+		 
+		meshFilter.mesh = meshBuilder.CreateMesh();
+		
+		MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
+		AddMaterials();
+		meshRenderer.materials = materialsList.ToArray();
+		
+		
+    }
+	private void AddMaterials(){
+		
+		Material redMat = new Material(Shader.Find("Specular"));
+		redMat.color = Color.red;
+		
+		Material blueMat = new Material(Shader.Find("Specular"));
+		blueMat.color = Color.blue;
+		
+		Material greenMat = new Material(Shader.Find("Specular"));
+		greenMat.color = Color.green;
+		
+		Material  magentaMat = new Material(Shader.Find("Specular"));
+		magentaMat.color = Color.magenta;
+		
+		Material yellowMat = new Material(Shader.Find("Specular"));
+		yellowMat.color = Color.yellow;
+		
+		Material whiteMat = new Material(Shader.Find("Specular"));
+		whiteMat.color = Color.white;
+		
+		materialsList = new List<Material>();
+		materialsList.Add(redMat);
+		materialsList.Add(blueMat);
+		materialsList.Add(greenMat);
+		materialsList.Add(magentaMat);
+		materialsList.Add(yellowMat);
+		materialsList.Add(whiteMat);
+	}
+
+}
---
+++ Assets/Scripts/MeshBuilder.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshBuilder 
+{
+    //defining variables
+	
+	private List<Vector3> vertices = new List<Vector3>();		//list of vertices to store mesh points
+	private List<int> indices = new List<int>(); 				//list of indices
+	private List<Vector3> normals = new List<Vector3>();		//list that defines direction of vertices
+	private List<Vector2> uvs = new List<Vector2>();			//uvs coordinates
+	private List<int>[] submeshIndices = new List<int>[] {};	//array of submesh indices
+	
+	
+	public MeshBuilder(int submeshCount){
+		
+		submeshIndices = new List<int>[submeshCount];
+		
+		for(int i = 0; i < submeshCount; i++){
+			 submeshIndices[i] = new List<int>();
+		}
+		
+	}
+	
+	public void BuildTriangle(Vector3 p0, Vector3 p1, Vector3 p2, int subMesh){
+		
+		Vector3 normal = Vector3.Cross(p1 - p0, p2 - p0).normalized;
+		
+		BuildTriangle(p0,p1,p2,normal,subMesh);
+		
+	}
+	
+	
+	// build triangle method
+	public void BuildTriangle(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 normal, int subMesh){
+		
+		//index of each vertex in list of vertices
+		int p0Index = vertices.Count;
+		int p1Index = vertices.Count + 1;
+		int p2Index = vertices.Count + 2;
+		
+		//add index of vertex to indices
+		indices.Add(p0Index);
+		indices.Add(p1Index);
+		indices.Add(p2Index);
+		
+		//
+		submeshIndices[subMesh].Add(p0Index);
+		submeshIndices[subMesh].Add(p1Index);
+		submeshIndices[subMesh].Add(p2Index);
+		
+		//add each point to our vertices list
+		vertices.Add(p0);
+		vertices.Add(p1);
+		vertices.Add(p2);
+		
+		// add normals to our normals list
+		normals.Add(normal);
+		normals.Add(normal);
+		normals.Add(normal);
+		
+		//add each UV coordinate to our uv list
+		uvs.Add(new Vector2(0,0));
+		uvs.Add(new Vector2(0,1));
+		uvs.Add(new Vector2(1,1));
+	}
+	
+
+	
+	// build Mesh method
+	public Mesh CreateMesh(){
+		
+		Mesh mesh = new Mesh();
+		
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = indices.ToArray();
+		mesh.normals = normals.ToArray();
+		mesh.uv = uvs.ToArray();
+		mesh.subMeshCount = submeshIndices.Length;
+		
+		for(int i =  0; i < submeshIndices.Length; i++){
+			
+			if(submeshIndices[i].Count < 3){
+				mesh.SetTriangles(new int[3] {0,0,0}, i);
+			}
+			else{
+				mesh.SetTriangles(submeshIndices[i].ToArray(), i);
+			}
+		}
+		
+		return mesh;
+	}
+	
+}
